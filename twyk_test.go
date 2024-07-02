e0deb8cf88f3e552b2ccfaba127ab489e767001a
Joumana El Alaoui <joumana.codes@gmail.com>
2024-07-02 03:05:46 -0500

    Adds a test to test Match

--- twyk_test.go
+++ twyk_test.go
@@ -1,1 +1,19 @@
 package twyk_test
+
+import (
+	"testing"
+
+	"github.com/joumanae/twyk"
+)
+
+func TestMatch(t *testing.T) {
+	// TODO: add more tests
+	got, err := twyk.Match("https://github.com/joumanae/twyk", "joumanae")
+	if err != nil {
+		t.Error(err)
+	}
+	want := true
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+}
